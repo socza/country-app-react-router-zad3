c8ba488241a78f4329f334fb9252c22ec8493aff
Piotr Sociński <psocinski@gmail.com>
2018-03-26 10:22:54 +0200

    Change className in tag <h1>

--- src/presentational/home.component.js
+++ src/presentational/home.component.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../country.css';
 
 const Home = () => (
-	<h1 className="adamOne">Witamy w aplikacji Państwa.js</h1>
+	<h1>Witamy w aplikacji Państwa.js</h1>
 );
 
 export default Home;
