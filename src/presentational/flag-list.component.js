a0820f460f538f7eb7e6317e51d282a9c90773fa
Piotr Sociński <psocinski@gmail.com>
2018-03-26 09:51:49 +0200

    Functionality: deleting the selected country - completed

--- src/presentational/flag-list.component.js
+++ src/presentational/flag-list.component.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router';
-import CountryFlag from './flag.component';
+import CountryFlag from '../presentational/flag.component';
 
 const CountryFlagList = (props) => (
     <div className="countries-list">
@@ -10,6 +10,7 @@ const CountryFlagList = (props) => (
                     <Link className='logo' to={'countries/country/' + country.id}>
                         <CountryFlag country={country} />
                     </Link>
+                    <button onClick={props.deleteCountry.bind(null, country.id)}>DELETE</button>
                 </div>
             )
         })}
